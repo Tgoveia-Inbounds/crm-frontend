0566ced68a8b3e819f134af4d818afd58067bca7
Thiago <tgoveia@inbounds.com>
2024-01-16 14:28:19 -0300

    chore: adding bulma and primevue

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import './assets/main.css'
+import 'primevue/resources/themes/lara-light-green/theme.css'
+import 'bulma/css/bulma.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +15,9 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue)
+
+app.component('PButton', Button)
+app.component('PInputText', InputText)
 
 app.mount('#app')
