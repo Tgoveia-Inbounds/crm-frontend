449e08f0da8b863aec6ee74276b5530a10c06a2e
Thiago <tgoveia@inbounds.com>
2024-01-16 14:29:11 -0300

    feat: add base login form view and component

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="login-form">
+    <form>
+      <label class="label" for="email-input">
+        Email
+        <p-input-text id="email-input" class="input" placeholder="Email" />
+      </label>
+      <label class="label" for="password-input">
+        Password
+        <p-input-text id="password-input" class="input" type="password" placeholder="Password" />
+      </label>
+      <p-button class="button is-info" label="Login" />
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'LoginForm'
+}
+</script>
+
+<style scoped>
+.login-form form {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
