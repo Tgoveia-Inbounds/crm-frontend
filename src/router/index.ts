3f446530caf13c4d33b7b663df92df0d3740acfd
Thiago <tgoveia@inbounds.com>
2024-01-17 08:05:59 -0300

    wip: adding menu layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginViewVue from '../views/LoginView.vue'
+// import UsersTableVue from '@/components/UsersTable.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/login',
-      name: 'home',
+      name: 'login',
       component: LoginViewVue
     }
+    // {
+    //   path: '/test',
+    //   name: 'test',
+    //   component: UsersTableVue
+    // }
   ]
 })
 
