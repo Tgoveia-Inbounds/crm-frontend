bb1d82c48b9b7b3fd8af37b267c0d14e0de71581
Thiago <tgoveia@inbounds.com>
2024-01-24 10:04:03 -0300

    feat: add campaign management ui

--- src/views/CampaignsView.vue
+++ src/views/CampaignsView.vue
@@ -1,3 +1,47 @@
 <template>
-  <h1>Campaigns</h1>
+  <div class="campaigns-view">
+    <div class="campaigns-header">
+      <h1>Campaigns</h1>
+      <p-button
+        class="button is-info create-campaign-button"
+        label="Create campaign"
+        title="Create campaign"
+        icon="pi pi-plus"
+        @click="() => $router.push({ name: 'create-campaign' })"
+      />
+    </div>
+    <campaigns-table />
+  </div>
 </template>
+
+<script setup lang="ts">
+import CampaignsTable from '@/components/CampaignsTable.vue'
+</script>
+
+<style scoped lang="scss">
+.campaigns-view {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 1rem;
+  align-self: center;
+  width: 100%;
+}
+.campaigns-view h1 {
+  color: #333;
+  margin: 1rem 0 1rem 1rem;
+  font-size: 2.5rem;
+  font-weight: 500;
+}
+.campaigns-header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.create-campaign-button {
+  width: 200px;
+  align-self: flex-end;
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+}
+</style>
